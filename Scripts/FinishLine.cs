c45227e4e9031df5b3f90b7073c9f87e1a339e3e
belfyhd <47932869+belfyhd@users.noreply.github.com>
2024-11-04 14:01:35 -0500

    Update FinishLine.cs

--- Scripts/FinishLine.cs
+++ Scripts/FinishLine.cs
@@ -8,6 +8,7 @@ public class FinishLine : MonoBehaviour
 
     public AudioSource finishAudio;
     public Rigidbody rb;
+    public AudioSource audio;
     // Start is called before the first frame update
     void Start()
     {
