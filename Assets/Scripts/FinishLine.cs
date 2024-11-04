cf76a11a54744ad1a1d9ce87173469f738c58e4b
belfyhd <47932869+belfyhd@users.noreply.github.com>
2024-11-04 14:18:02 -0500

    BoostGame

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -8,7 +8,6 @@ public class FinishLine : MonoBehaviour
 
     public AudioSource finishAudio;
     public Rigidbody rb;
-    public AudioSource audio;
     // Start is called before the first frame update
     void Start()
     {
